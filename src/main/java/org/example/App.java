cd71d27f26a7371869be380ec3b10f9d26f7b10d
Rajesh <rajeshmanik721211@gmail.com>
2026-01-29 22:08:14 +0530

    DependencyInjectionFinalCommit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class App {
+     public static void main(String args[])
+     {
+
+
+        /*Dependency Injection(Setter) 8,9 Line Through setter (Manually Injection) , Using SpringCore We Automatically Handel It
+                We Inject Dependency Through SetterMethod so, it called SetterDependencyInjection
+
+         Student student = new Student();
+         Course course = new Dsa();
+         student.setCourse(course);
+         */
+
+          /*Dependency Injection(Constructor) 8,9 Line Through Constructor (Manually Injection) Using SpringCore We Automatically Handel It
+                We Inject Dependency Through Constructor so, it called Constructor)DependencyInjection
+
+         Student student = new Student( new JavaFullStack());
+         student.Study();
+
+           */
+     }
+}
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -3,4 +3,35 @@ package org.example;
 public class Student {
 
     private Course course;
+
+    public Student()
+    {
+
+    }
+    /**Constructor Injection **/
+    public Student(Course course)
+    {
+        this.course = course;
+    }
+
+    //To Remove The NullPointerException use Dependency Injection
+
+    /** setter  injection : Use Setter Method**/
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+
+
+    public void Study()
+    {
+        int start = course.enroll();
+        if(start == 1)
+        {
+            System.out.println("Journey Stated");
+        }
+        else{
+            System.out.println("Payment Failed");
+        }
+    }
 }
